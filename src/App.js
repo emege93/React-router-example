d74c1bd419926da8f461a26d6ab17c8df2b3677d
emege93 <emege93@gmail.com>
2020-10-26 18:02:42 +0100

    Router y hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
 import './App.css';
+import Contacto from "./components/Contacto";
+import Inicio from "./components/Inicio";
+import Nosotros from "./components/Nosotros";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-5">
+        <div className="btn-group">
+          <Link to="/" className="btn btn-dark">
+            Inicio
+          </Link>
+          <Link to="/contacto" className="btn btn-dark">
+            Contacto
+          </Link>
+          <NavLink to="/nosotros" className="btn btn-dark" activeClassName="active">
+            Nosotros
+          </NavLink>
+        </div>
+        <hr/>
+        <Switch>
+          <Route path="/" exact>
+            <Inicio/>
+          </Route>
+          <Route path="/contacto">
+            <Contacto/>
+          </Route>
+          <Route path="/nosotros">
+            <Nosotros/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
